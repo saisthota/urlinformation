b4bdace87f620125dc03a4d2f0042c4cbc3a5712
saisthota <me@saisthota.com>
2016-09-25 16:06:38 -0700

    Fetch Title and Description functionality

--- index.js
+++ index.js
@@ -9,9 +9,23 @@ server.use(bodyParser.urlencoded({ extended: true }));
 /* Set a route for listening request */
 server.post('api/fetch', function(req, res, next) {
    //res.send(200, JSON.stringify("Response: "+req.body.url));
-    http('http://www.google.com', function (error, response, body) {
+    http('http://www.github.com/saisthota', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log(body) // Show the HTML for the Google homepage.
+            /* Convert response to String */
+            var data = body.toString();
+
+            /* Get Title */
+            var title = new RegExp("<title>(.*?)</title>", "i");
+
+            /* Get Description */
+            var description = new RegExp('<meta name="description" content="(.*)">');
+
+            console.log(data.match(title)[1]);
+            console.log(data.match(description)[1]);
+            res.send(JSON.stringify({"title": title}));
+        }
+        else {
+            console.log("Error");
         }
     })
 });
