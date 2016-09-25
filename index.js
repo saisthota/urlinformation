04f42a4dee66cac22bd0d956a0f90156895218dc
saisthota <me@saisthota.com>
2016-09-25 15:40:43 -0700

    Added request

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var rest = require('restify');
 var server = rest.createServer();
+var http = require('request');
 
 var bodyParser = require('body-parser');
 server.use(bodyParser.json());
@@ -7,9 +8,13 @@ server.use(bodyParser.urlencoded({ extended: true }));
 
 /* Set a route for listening request */
 server.post('api/fetch', function(req, res, next) {
-   res.send(200, JSON.stringify("Response: "+req.body.url));
+   //res.send(200, JSON.stringify("Response: "+req.body.url));
+    http('http://www.google.com', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body) // Show the HTML for the Google homepage.
+        }
+    })
 });
-
 
 /* Set Server to listen on PORT 3000 */
 server.listen(3000, function() {
